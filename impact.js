d56b8b5e8436ec294aae1735142ecb5897def7c1
Benjamin Fenton <fenton06@gmail.com>
2017-12-08 23:13:57 -0600

    TITTIES IN YA FACE

--- impact.js
+++ impact.js
@@ -12,7 +12,7 @@ var objectMaterial = new THREE.MeshPhongMaterial({
     transparent: true
 });
 
-var lineMaterial = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe:true, side: THREE.BackSide});
+var lineMaterial = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true, side: THREE.BackSide});
 
 
 var ourCanvas;
@@ -22,11 +22,9 @@ var solidShapeShape;
 var fragments = [];
 var leftShape;
 
-var drawLines = false; // TODO boostrap switch
-var animate = true; // TODO boostrap switch
-var experimental = false; // TODO boostrap switch
-// TODO make buttons for everything in handleKeyPress
-// TODO update frame counter under canvas 
+var animate = true;
+var drawLines = false;
+var experimental = false;
 
 var impactPoint;
 var fragmentSpeedVariable = .009;
@@ -39,14 +37,18 @@ var frameIncrement = 3;
 var camera;
 var infoTable;
 
-function onDocumentMouseClick(event) {
+function onDocumentMouseClick() {
     frameNumber = 0;
     // Get values
     var impactForce = parseInt(document.getElementById("impactForceVal").innerHTML);
     var materialStrength = parseInt(document.getElementById("materialStrengthVal").innerHTML);
     var appendageCount = parseInt(document.getElementById("crackRangeVal").innerHTML);
+
+    animate = $("#animateCheckbox").prop('checked');
+    drawLines = $("#drawLinesCheckbox").prop('checked');
+    experimental = $("#experimentalCheckbox").prop('checked');
+
     var numOfShatterLayers = Math.floor(randomFromInterval(3, 10));
-    
 
     var inWidth = impactPoint.position.x > -width && impactPoint.position.x < width;
     var inHeight = impactPoint.position.y > -height && impactPoint.position.y < height;
@@ -59,7 +61,7 @@ function onDocumentMouseClick(event) {
         if (impactForce >= materialStrength) {
             shatterPercent = 1.0;
         } else {
-            shatterPercent = (Math.random() * (1 - (materialStrength - impactForce)/100.0)).toFixed(4);
+            shatterPercent = (Math.random() * (1 - (materialStrength - impactForce) / 100.0)).toFixed(4);
         }
 
         var shatterDistance = height > width ? height * shatterPercent : width * shatterPercent;
@@ -81,20 +83,19 @@ function onDocumentMouseClick(event) {
 
         // generate layer 1
         info("<b>Layer 1</b>");
-        if(experimental){
+        if (experimental) {
             var total = Math.pow(numOfShatterLayers, 2);
-            var percent = 1/total;
+            var percent = 1 / total;
             shatterLayers.push(generateLayer(startPoint, startAngle, shatterDistance * percent, appendageCount));
         }
-        else{
+        else {
             shatterLayers.push(generateLayer(startPoint, startAngle, shatterLayerDistance, appendageCount));
         }
-        
-        
+
 
         // generate layer 1 shapes
         var gL = shatterLayers[0].length;
-        for(var i = 0; i < gL; ++i){
+        for (var i = 0; i < gL; ++i) {
             var shape = new THREE.Shape();
             shape.moveTo(
                 startPoint['x'],
@@ -103,78 +104,78 @@ function onDocumentMouseClick(event) {
 
             shape.lineTo(
                 cW(shatterLayers[0][i]['x']),
-                 cH(shatterLayers[0][i]['y']),
-                  .1);
+                cH(shatterLayers[0][i]['y']),
+                .1);
 
             shape.lineTo(
                 cW(shatterLayers[0][i + 1 >= gL ? 0 : i + 1]['x']),
                 cH(shatterLayers[0][i + 1 >= gL ? 0 : i + 1]['y']),
-                  .1);
-    
+                .1);
+
             var t_mesh = new THREE.Mesh(new THREE.ShapeGeometry(shape), drawLines ? lineMaterial : objectMaterial);
-        
+
             scene.add(t_mesh);
             fragments.push(t_mesh);
         }
 
 
         // generate layer n shapes
-        for(var i = 1; i <= numOfShatterLayers; ++i){
-            info("<b>Layer: " + (i+1) + "</b>");
-            if(experimental && i == numOfShatterLayers){
-                shatterLayers.push(generateLayer(startPoint, startAngle, (shatterLayerDistance * i) + randomFromInterval(0,shatterLayerDistance), appendageCount))
-            }else{
-                if(experimental){
-                    var total = Math.pow(numOfShatterLayers, 2);
+        for (i = 1; i <= numOfShatterLayers; ++i) {
+            info("<b>Layer: " + (i + 1) + "</b>");
+            if (experimental && i === numOfShatterLayers) {
+                shatterLayers.push(generateLayer(startPoint, startAngle, (shatterLayerDistance * i) + randomFromInterval(0, shatterLayerDistance), appendageCount))
+            } else {
+                if (experimental) {
+                    total = Math.pow(numOfShatterLayers, 2);
                     var part = Math.pow(i, 2);
-                    var percent = part/total;
-                    shatterLayers.push(generateLayer(startPoint, startAngle, percent*shatterDistance, appendageCount))
-                }else{
+                    percent = part / total;
+                    shatterLayers.push(generateLayer(startPoint, startAngle, percent * shatterDistance, appendageCount))
+                } else {
                     shatterLayers.push(generateLayer(startPoint, startAngle, (shatterLayerDistance * i), appendageCount))
                 }
-                
+
             }
         }
 
         // create layer n shapes and lines 
         gL = shatterLayers.length - 1;
         // for each layer after first layer
-        for(var l = 1; l <= gL; ++l){
+        for (var l = 1; l <= gL; ++l) {
             var LL = shatterLayers[l].length;
-            for(var i = 0; i < LL; ++i){
-                
+            for (i = 0; i < LL; ++i) {
+
                 var p1 = shatterLayers[l - 1][i];
                 var p2 = shatterLayers[l][i];
                 var p3 = shatterLayers[l][i + 1 >= LL ? 0 : i + 1];
-                var p4 = shatterLayers[l -1][i + 1 >= shatterLayers[l -1].length ? 0 : i + 1];
+                var p4 = shatterLayers[l - 1][i + 1 >= shatterLayers[l - 1].length ? 0 : i + 1];
 
-                var shape = new THREE.Shape();
+                shape = new THREE.Shape();
                 shape.moveTo(
                     cW(p1['x']),
                     cH(p1['y']),
                     .1);
 
-                if(experimental){
+                if (experimental) {
 
                     shape.lineTo(
-                        cW(p2['x']), 
+                        cW(p2['x']),
                         cH(p2['y']),
-                          .1);
-        
+                        .1);
+
                     shape.lineTo(
                         cW(p3['x']),
                         cH(p3['y']),
-                          .1);
+                        .1);
 
-                    try{
-                        var t_mesh = new THREE.Mesh(new THREE.ShapeGeometry(shape), drawLines ? lineMaterial : objectMaterial);
+                    try {
+                        t_mesh = new THREE.Mesh(new THREE.ShapeGeometry(shape), drawLines ? lineMaterial : objectMaterial);
                         scene.add(t_mesh);
                         fragments.push(t_mesh);
-                    }catch(err){
+                    } catch (err) {
                         console.error(err);
                     }
 
-                    var shape = new THREE.Shape();
+                    shape = new THREE.Shape();
                     shape.moveTo(
                         cW(p1['x']),
                         cH(p1['y']),
@@ -184,41 +185,41 @@ function onDocumentMouseClick(event) {
                         cW(p3['x']),
                         cH(p3['y']),
                         .1);
-    
+
                     shape.lineTo(
                         cW(p4['x']),
                         cH(p4['y']),
                         .1);
-            
-                    try{
-                        var t_mesh = new THREE.Mesh(new THREE.ShapeGeometry(shape), drawLines ? lineMaterial : objectMaterial);
+
+                    try {
+                        t_mesh = new THREE.Mesh(new THREE.ShapeGeometry(shape), drawLines ? lineMaterial : objectMaterial);
                         scene.add(t_mesh);
                         fragments.push(t_mesh);
-                    }catch(err){
+                    } catch (err) {
                         console.error(err);
                     }
 
-                }else{
+                } else {
                     shape.lineTo(
-                        cW(p2['x']), 
+                        cW(p2['x']),
                         cH(p2['y']),
-                          .1);
-        
+                        .1);
+
                     shape.lineTo(
                         cW(p3['x']),
                         cH(p3['y']),
-                          .1);
-    
+                        .1);
+
                     shape.lineTo(
                         cW(p4['x']),
                         cH(p4['y']),
                         .1);
-            
-                    try{
-                        var t_mesh = new THREE.Mesh(new THREE.ShapeGeometry(shape), drawLines ? lineMaterial : objectMaterial);
+
+                    try {
+                        t_mesh = new THREE.Mesh(new THREE.ShapeGeometry(shape), drawLines ? lineMaterial : objectMaterial);
                         scene.add(t_mesh);
                         fragments.push(t_mesh);
-                    }catch(err){
+                    } catch (err) {
                         console.error(err);
                     }
                 }
@@ -228,8 +229,8 @@ function onDocumentMouseClick(event) {
 
         // generate 'large' ouside piece
         var vectors = [];
-        shatterLayers[shatterLayers.length-1].forEach(function(point){
-            vectors.push(new THREE.Vector3(cW(point['x']),cH(point['y']),.1));
+        shatterLayers[shatterLayers.length - 1].forEach(function (point) {
+            vectors.push(new THREE.Vector3(cW(point['x']), cH(point['y']), .1));
         });
         var path = new THREE.Path(vectors);
         scene.remove(solidShape);
@@ -239,96 +240,91 @@ function onDocumentMouseClick(event) {
 
 
         // Fragment Movement
-        fragments.forEach(function(fragment){
-         animateFragment(fragment, startPoint)  
+        fragments.forEach(function (fragment) {
+            animateFragment(fragment, startPoint)
         });
     }
 }
 
-function animateFragment(fragment, startPoint){
+function animateFragment(fragment, startPoint) {
     var x_sum = 0;
     var y_sum = 0;
-    fragment.geometry.vertices.forEach(function(vertex){
+    fragment.geometry.vertices.forEach(function (vertex) {
         x_sum += vertex.x;
         y_sum += vertex.y;
     });
 
     var x_avg = x_sum / fragment.geometry.vertices.length;
     var y_avg = y_sum / fragment.geometry.vertices.length;
-    
-    if(drawLines){
+
+    if (drawLines) {
         var dotGeometry = new THREE.Geometry();
-        dotGeometry.vertices.push(new THREE.Vector3( x_avg, y_avg, .01));
-        var dot = new THREE.Points( dotGeometry, lineMaterial );
-        scene.add( dot );
+        dotGeometry.vertices.push(new THREE.Vector3(x_avg, y_avg, .01));
+        var dot = new THREE.Points(dotGeometry, lineMaterial);
+        scene.add(dot);
     }
 
-    var fragment_center = {x: x_avg, y: y_avg };
+    var fragment_center = {x: x_avg, y: y_avg};
     var distanceFromCenter = pointDistance(startPoint, fragment_center);
     var angleDeg = to_positive_angle(Math.atan2(y_avg - startPoint['y'], x_avg - startPoint['x']) * 180 / Math.PI);
-    var moveSpinPerc = 1/(1 + Math.pow(distanceFromCenter,2));
-    fragment['moveSpinPerc'] = moveSpinPerc;
+    fragment['moveSpinPerc'] = 1 / (1 + Math.pow(distanceFromCenter, 2));
     fragment['spinAngle'] = angleDeg;
 }
 
 
-function generateLayer(point, startAngle, distance, points){
+function generateLayer(point, startAngle, distance, points) {
     var layer = [];
-    var increment = 360/points;
-    for(var i = 0; i < points; ++i){
+    var increment = 360 / points;
+    for (var i = 0; i < points; ++i) {
         var rads = toRadians(startAngle + (increment * i));
         var riseAmnt = distance * Math.sin(rads);
         var runAmnt = distance * Math.cos(rads);
-        if(experimental){
+        if (experimental) {
             runAmnt = runAmnt + (Math.random() * .25 * runAmnt);
             riseAmnt = riseAmnt + (Math.random() * .25 * riseAmnt);
             layer.push({x: point['x'] + runAmnt, y: point['y'] + riseAmnt});
             info("Point: " + (point['x'] + runAmnt).toFixed(6) + " " + (point['y'] + riseAmnt).toFixed(6) + " Angle: " + (startAngle + (increment * i)));
-        }else{
+        } else {
             layer.push({x: point['x'] + runAmnt, y: point['y'] + riseAmnt});
             info("Point: " + (point['x'] + runAmnt).toFixed(6) + " " + (point['y'] + riseAmnt).toFixed(6) + " Angle: " + (startAngle + (increment * i)));
         }
-        
+
     }
 
     return layer;
 }
 
-function cW(val){
+function cW(val) {
     return val >= width ? width : val < -width ? -width : val;
 }
 
-function cH(val){
+function cH(val) {
     return val >= height ? height : val < -height ? -height : val;
 }
 
 
-
-function pointDistance(p1, p2){
-    return Math.sqrt(Math.pow(p2['x'] - p1['x'],2) + Math.pow(p2['y'] - p1['y'],2));
+function pointDistance(p1, p2) {
+    return Math.sqrt(Math.pow(p2['x'] - p1['x'], 2) + Math.pow(p2['y'] - p1['y'], 2));
 }
 
 
-function toRadians (angle) {
-  return angle * (Math.PI / 180);
+function toRadians(angle) {
+    return angle * (Math.PI / 180);
 }
 
 
-function randomFromInterval(min,max)
-{
-    return (Math.random()*(max-min+1)+min);
+function randomFromInterval(min, max) {
+    return (Math.random() * (max - min + 1) + min);
 }
 
-function to_positive_angle(angle)
-{
-   angle = angle % 360;
-   while(angle < 0) { 
-     angle += 360.0;
-   }
+function to_positive_angle(angle) {
+    angle = angle % 360;
+    while (angle < 0) {
+        angle += 360.0;
+    }
 
-   return angle;
+    return angle;
 }
-
 
 
 function getChar(event) {
@@ -343,30 +339,33 @@ function getChar(event) {
 
 function handleKeyPress(event) {
     var ch = getChar(event);
-    if(ch == ']'){
-        frameNumber ++;
+    if (ch === ']') {
+        frameNumber++;
     }
-    if(ch == '['){
-        frameNumber --;
-        if(frameNumber < 0){
+    if (ch === '[') {
+        frameNumber--;
+        if (frameNumber < 0) {
             frameNumber = 0;
         }
     }
 
-    if(ch == 'H'){
+    if (ch === 'H') {
         drawLines = !drawLines;
+        $("#drawLinesCheckbox").bootstrapToggle('toggle');
     }
 
-    if(ch == 'R'){
+    if (ch === 'R') {
         reset();
     }
 
-    if(ch == 'E'){
+    if (ch === 'E') {
         experimental = !experimental;
+        $("#experimentalCheckbox").bootstrapToggle('toggle');
     }
 
-    if(ch == ' '){
+    if (ch === ' ') {
         animate = !animate;
+        $("#animateCheckbox").bootstrapToggle('toggle');
     }
 
     cameraControl(camera, ch);
@@ -397,26 +396,26 @@ function onDocumentMouseMove(event) {
 
 }
 
-function info(string){
-    infoTable.innerHTML += "<br/>"+ string;
+function info(string) {
+    infoTable.innerHTML += "<br/>" + string;
 }
 
-function reset(){
+function reset() {
     infoTable = document.getElementById("infoTable");
     infoTable.innerHTML = "";
 
-    fragments.forEach(function(fragment){
+    fragments.forEach(function (fragment) {
         scene.remove(fragment);
     });
 
     fragments = [];
     isBroken = false;
 
-    if(solidShape){
+    if (solidShape) {
         scene.remove(solidShape);
     }
 
-    if(leftShape){
+    if (leftShape) {
         scene.remove(leftShape);
     }
 
@@ -435,6 +434,25 @@ function reset(){
 function start() {
 
     window.onkeypress = handleKeyPress;
+
+    if(animate) {
+        $("#animateCheckbox").bootstrapToggle('on');
+    } else {
+        $("#animateCheckbox").bootstrapToggle('off');
+    }
+
+    if(drawLines) {
+        $("#drawLinesCheckbox").bootstrapToggle('on');
+    } else {
+        $("#drawLinesCheckbox").bootstrapToggle('off');
+    }
+
+    if(experimental) {
+        $("#experimentalCheckbox").bootstrapToggle('on');
+    } else {
+        $("#experimentalCheckbox").bootstrapToggle('off');
+    }
+
     document.addEventListener('mousemove', onDocumentMouseMove, false);
     document.addEventListener("click", onDocumentMouseClick);
 
@@ -469,27 +487,27 @@ function start() {
 
 
     var render = function () {
-        if(isBroken && lastFrame != frameNumber){
-            fragments.forEach(function(fragment){
-                fragment.position.z = -(frameNumber * (fragmentSpeedVariable )) * fragment['moveSpinPerc'];
+        if (isBroken && lastFrame !== frameNumber) {
+            fragments.forEach(function (fragment) {
+                fragment.position.z = -(frameNumber * (fragmentSpeedVariable)) * fragment['moveSpinPerc'];
 
                 var riseAmnt = fragment['moveSpinPerc'] * Math.sin(toRadians(fragment['spinAngle']));
                 var runAmnt = fragment['moveSpinPerc'] * Math.cos(toRadians(fragment['spinAngle']));
 
                 fragment.rotation.y = (frameNumber * riseAmnt * fragmentRotationVariable);
                 fragment.rotation.x = (frameNumber * runAmnt * fragmentRotationVariable);
-                
+
                 lastFrame = frameNumber;
             });
         }
 
-        if(animate){
+        if (animate) {
             frameNumber += frameIncrement;
-            if(frameNumber > 1000 && frameNumber < 1500){
+            if (frameNumber > 1000 && frameNumber < 1500) {
                 isBroken = false;
             }
 
-            if(frameNumber > 1500){
+            if (frameNumber > 1500) {
                 frameNumber = 0;
                 isBroken = true;
             }
@@ -497,6 +515,7 @@ function start() {
 
         renderer.render(scene, camera);
         requestAnimationFrame(render);
+        document.getElementById("frameNum").innerHTML = frameNumber.toString();
     };
 
     render();
